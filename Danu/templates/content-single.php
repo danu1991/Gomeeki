18e7b00499639617f7f89575493bcc7b013377d6
Danu <danushkaj91@gmail.com>
2016-12-21 17:52:04 +0700

    posts

--- Danu/templates/content-single.php
+++ Danu/templates/content-single.php
@@ -1,15 +1,31 @@
 <?php while (have_posts()) : the_post(); ?>
+<?php 
+	$thumb_id = get_post_thumbnail_id();
+	$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+	$thumb_url = $thumb_url_array[0];
+?>
+	<div class="header-img" style="background-image:url('<?php echo $thumb_url;?>');">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="page-title-banner ">
+						<div class="page-title-banner-head blog-banner"><?php echo get_the_title();?></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
   <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <div class="container post-body">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</div>
+   
   </article>
 <?php endwhile; ?>
+
+
