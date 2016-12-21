d81e103c7d993fca7eb4b399ad68420d52bf9209
Danu <danushkaj91@gmail.com>
2016-12-21 16:04:01 +0700

    Footer
    Footer and css updates

--- Danu/templates/footer.php
+++ Danu/templates/footer.php
@@ -1,6 +1,20 @@
 <footer class="content-info">
-  <div class="container">
+  <div class="container-fluid">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-	<?php echo do_shortcode("[DISPLAY_ULTIMATE_SOCIAL_ICONS]"); ?>
+	<div class="row">
+		<div class="col-md-12">
+			<hr />
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<?php echo do_shortcode("[DISPLAY_ULTIMATE_SOCIAL_ICONS]"); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<p class="copyright text-muted">Copyright &copy; Danu <?php echo date("Y");?></p>
+		</div>
+	</div>
   </div>
 </footer>
